c1b09fa01b8b0be9257d724af2f2e89f8ce66501
Gabriel Fresatto <gabrielfresatto@gmail.com>
2019-06-05 11:13:54 -0300

    Updates

---
+++ precache-manifest.9eafbfb63292b6026918b39920368c17.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd9ec4f2fb1497069556442e6bf1f2e",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "5ca2bf5518cf3fdcc47b",
+    "url": "/spotify/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "3ed0c615abbc08a7b9ec",
+    "url": "/spotify/static/css/main.2baf2a8d.chunk.css"
+  },
+  {
+    "revision": "5ca2bf5518cf3fdcc47b",
+    "url": "/spotify/static/js/2.143f7137.chunk.js"
+  },
+  {
+    "revision": "3ed0c615abbc08a7b9ec",
+    "url": "/spotify/static/js/main.86c8d43f.chunk.js"
+  },
+  {
+    "revision": "024922446ac752300378",
+    "url": "/spotify/static/js/runtime~main.d74fabdd.js"
+  },
+  {
+    "revision": "3f6e21d5b7262e386bc99db029dca18c",
+    "url": "/spotify/static/media/add_playlist.3f6e21d5.svg"
+  },
+  {
+    "revision": "198550c154cb26037ac4ec6b3a1fd664",
+    "url": "/spotify/static/media/backward.198550c1.svg"
+  },
+  {
+    "revision": "63fca2afee55e0fcfd8975b787a3908f",
+    "url": "/spotify/static/media/forward.63fca2af.svg"
+  },
+  {
+    "revision": "6557e51e1e6d913c5a89e62b2791cdc2",
+    "url": "/spotify/static/media/play.6557e51e.svg"
+  },
+  {
+    "revision": "716dfa0b41f0008fced2daef09f22aeb",
+    "url": "/spotify/static/media/repeat.716dfa0b.svg"
+  },
+  {
+    "revision": "54850ef47ae6d4d038552c89fafeb57b",
+    "url": "/spotify/static/media/shuffle.54850ef4.svg"
+  },
+  {
+    "revision": "6b27f5927a792a595f190dda190747d6",
+    "url": "/spotify/static/media/volume.6b27f592.svg"
+  }
+]);
